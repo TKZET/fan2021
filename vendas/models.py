ccff88446f8782e899d360deff26a24dddc3c510
TKZET <tahaktobias@gmail.com>
2021-03-30 13:15:46 -0300

    Adicionando os Verbose_Names

--- vendas/models.py
+++ vendas/models.py
@@ -3,18 +3,18 @@ from django.db import models
 
 # Create your models here.
 class Venda(models.Model):
-    nome = models.CharField(max_length=255, null=False, blank=False)
-    valor = models.DecimalField(max_digits=12, decimal_places=2, null=False, blank=False)
+    nome = models.CharField(max_length=255, null=False, blank=False, verbose_name='Nome da Venda')
+    valor = models.DecimalField(max_digits=12, decimal_places=2, null=False, blank=False,
+                                verbose_name='Valor total da Venda')
     data_venda = models.DateField(auto_now_add=True, blank=True, null=False)
     hora_venda = models.TimeField(auto_now_add=True, blank=True, null=False)
     data_hora_venda = models.DateTimeField(auto_now_add=True, blank=True, null=False)
-    numero_venda = models.IntegerField(blank=False, null=False)
-    observacao = models.TextField(blank=True, null=True)
-    comprovante_venda = models.FileField(blank=True, null=True)
+    numero_venda = models.IntegerField(blank=False, null=False, verbose_name='Número da venda')
+    observacao = models.TextField(blank=True, null=True, verbose_name='Observação')
+    comprovante_venda = models.FileField(blank=True, null=True, verbose_name='Comprovante de venda')
     exemplo_upload = models.FileField(upload_to='outro_diretorio/', null=True, blank=True)
-    email_cliente = models.EmailField(blank=True, null=True)
     venda_concluida = models.BooleanField(blank=False, null=False)
-    quantidade_itens = models.IntegerField(blank=True, null=False)
+    quantidade_itens = models.IntegerField(blank=True, null=False, verbose_name='Quantidade de Itens')
     produtos = models.ManyToManyField('Produto')
     cliente = models.ForeignKey('Cliente', on_delete=models.DO_NOTHING, default=1)
 
@@ -33,8 +33,10 @@ class Produto(models.Model):
 
 
 class Cliente(models.Model):
-    nome = models.CharField(max_length=255, blank=False, null=False)
-    cpf = models.CharField(max_length=11, blank=False, null=False)
+    nome = models.CharField(max_length=255, blank=False, null=False, verbose_name='Nome Completo')
+    cpf = models.CharField(max_length=11, blank=False, null=False, verbose_name='CPF')
+    email_cliente = models.EmailField(blank=True, null=True, verbose_name='Email')
+
 
     def __str__(self):
         return self.nome
